34176abe88e8878ef4813baa7fbaed01e6f59f22
Eduardo rauber munis <eduardomunis10@gmail.com>
2025-06-05 21:17:16 -0300

    feat

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
 // Scroll suave para âncoras
 document.querySelectorAll("a[href^='#']").forEach((link) => {
   link.addEventListener("click", (e) => {
     e.preventDefault();
     const id = link.getAttribute("href");
-    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    const destino = document.querySelector(id);
+    if (destino) {
+      destino.scrollIntoView({ behavior: "smooth" });
+    }
   });
 });
 
 // Simulação de envio de formulário
-document.getElementById("form").addEventListener("submit", function (e) {
-  e.preventDefault();
-  alert("Mensagem enviada com sucesso!");
-  this.reset();
+const form = document.getElementById("form");
+if (form) {
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Mensagem enviada com sucesso!");
+    this.reset();
+  });
+}
+
+const lightbox = GLightbox({
+  selector: ".glightbox",
 });
